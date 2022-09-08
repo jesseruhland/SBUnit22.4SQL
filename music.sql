d7aa961e1af660d5d10c8e2f352e729093273974
Jesse Ruhland <jesse.ruhland@gmail.com>
2022-09-08 14:11:56 -0400

    assignment complete

--- music.sql
+++ music.sql
@@ -28,15 +28,13 @@ CREATE TABLE songs
   title TEXT NOT NULL,
   duration_in_seconds INTEGER NOT NULL,
   release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+  album_id INT NOT NULL REFERENCES albums(id)
 );
 
 CREATE TABLE song_artists (
   id SERIAL PRIMARY KEY,
   song_id INTEGER REFERENCES songs(id),
-  artist_id INTEGER REFERENCES artist(id)
+  artist_id INTEGER REFERENCES artists(id)
 );
 
 CREATE TABLE song_producers (
@@ -66,18 +64,76 @@ VALUES
 INSERT INTO albums (name)
 VALUES
   ('Middle of Nowhere'),
-  ('')
+  ('A Night at the Opera'),
+  ('Daydream'),
+  ('A Star Is Born'),
+  ('Silver Side Up'),
+  ('The Blueprint 3'),
+  ('Prism'),
+  ('Hands All Over'),
+  ('Let Go'),
+  ('The Writing''s on the Wall');
+
+INSERT INTO producers (name)
+VALUES
+  ('Dust Brothers'),
+  ('Stephen Lironi'),
+  ('Roy Thomas Baker'),
+  ('Walter Afanasieff'),
+  ('Benjamin Rice'),
+  ('Rick Parashar'),
+  ('Al Shux'),
+  ('Max Martin'),
+  ('Cirkut'),
+  ('Shellback'),
+  ('Benny Blanco'),
+  ('The Matrix'),
+  ('Darkchild');
 
 INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
+  (title, duration_in_seconds, release_date, album_id)
 VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+  ('MMMBop', 238, '04-15-1997', 1),
+  ('Bohemian Rhapsody', 355, '10-31-1975', 2),
+  ('One Sweet Day', 282, '11-14-1995', 3),
+  ('Shallow', 216, '09-27-2018', 4),
+  ('How You Remind Me', 223, '08-21-2001', 5),
+  ('New York State of Mind', 276, '10-20-2009', 6),
+  ('Dark Horse', 215, '12-17-2013', 7),
+  ('Moves Like Jagger', 201, '06-21-2011', 8),
+  ('Complicated', 244, '05-14-2002', 9),
+  ('Say My Name', 240, '11-07-1999', 10);
+
+INSERT INTO song_artists (song_id, artist_id)
+VALUES
+  (1,1),
+  (2,2),
+  (3,3),
+  (3,4),
+  (4,5),
+  (4,6),
+  (5,7),
+  (6,8),
+  (6,9),
+  (7,10),
+  (7,11),
+  (8,12),
+  (8,13),
+  (9,14),
+  (10,15);
+
+INSERT INTO song_producers (song_id, producer_id)
+VALUES
+  (1,1),
+  (1,2),
+  (2,3),
+  (3,4),
+  (4,5),
+  (5,6),
+  (6,7),
+  (7,8),
+  (7,9),
+  (8,10),
+  (8,11),
+  (9,12),
+  (10,13);
